51924e30b69138d22dc9d190e5925269e9d1d370
milliarder <you@example.com>
2026-07-03 23:04:27 +0700

    Rename notification channel: 'Kizzy RPC' → 'Kizzy Enhanced RPC'

--- feature_rpc_base/src/main/java/com/my/kizzy/feature_rpc_base/Constants.kt
+++ feature_rpc_base/src/main/java/com/my/kizzy/feature_rpc_base/Constants.kt
@@ -14,7 +14,7 @@ package com.my.kizzy.feature_rpc_base
 
 object Constants {
     const val CHANNEL_ID = "kizzy.notification"
-    const val CHANNEL_NAME = "Kizzy RPC"
+    const val CHANNEL_NAME = "Kizzy Enhanced RPC"
     const val CHANNEL_DESCRIPTION = "Background Service notification which runs rpc"
     const val NOTIFICATION_ID = 2022_03_04
     const val ACTION_STOP_SERVICE = "Stop Service"
